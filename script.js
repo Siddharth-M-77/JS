81eeebf9fe4f88bea08d34f90986519b247a6f68
Siddharth-M-77 <siddharthmaddheshiya777@gmail.com>
2024-09-10 08:08:17 -0700

    naming convention

--- script.js
+++ script.js
@@ -7,10 +7,24 @@ console.log("helooo");
 console.error("helooo");
 console.warn("helooo");
 
-
 // variables=>variable is basically a container where we can store diffrent type of data ,like(string,number,boolean,array,object etc)....
 
-var string = "hey sidd"
+var string = "hey sidd";
 console.log(string);
 
 
+// = is basically a assignment opreator by the help of this opreator we can assign any value to a variable.
+
+//left side of = is variable name and variable and the right side of = is value
+
+
+//Naming convention  of the variables
+//1. variable must start with small/capital /_ and not must be started number ,any-special character
+
+//2. variable must be in a single word 
+
+//3.    camelCase=> helloBro
+//      snake_case=> hello_bro
+//      PascalCase=> HelloBro
+//      kabab-case=> hello-bro
+   
