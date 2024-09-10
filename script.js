1ecbdb3d01427db8786e766a1ec895cef8f98aab
Siddharth-M-77 <siddharthmaddheshiya777@gmail.com>
2024-09-10 07:51:31 -0700

    variable

--- script.js
+++ script.js
@@ -3,3 +3,14 @@
 // keyword=> any word which any meaning in JavaScript is called keywords
 // word=> any word which any meaning is not  in JavaScript is called keywords
 
+console.log("helooo");
+console.error("helooo");
+console.warn("helooo");
+
+
+// variables=>variable is basically a container where we can store diffrent type of data ,like(string,number,boolean,array,object etc)....
+
+var string = "hey sidd"
+console.log(string);
+
+
