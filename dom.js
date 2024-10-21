eba919b7404ab036bf2d3959df8700cdacada879
Siddharth-M-77 <siddharthmaddheshiya777@gmail.com>
2024-10-21 07:59:28 -0700

    DOM

--- dom.js
+++ dom.js
@@ -1,11 +1,21 @@
-//document=> document is the pre defined Object given by the JavaScript  which holds the connected html file(index.html) 
+// 🚀Document Object Model(DOM)🚀
 
-console.log(document)
+//document=> document is the pre defined Object given by the JavaScript  which holds the connected html file(index.html)
+
+console.log(document);
 //id=> document.getElementById()
 //class=>document.getElementByClassName()
 //tag=>document.getELementByTagName()
 
 //universal selector
 
-//document.querySelector()=>to select the first;/Single element 
+//document.querySelector()=>to select the first;/Single element
 //document.querySelectorAll()=>to select the multiple element
+
+let heading = document.querySelector("h1");
+console.log(heading);
+console.log(heading.innerHTML);
+console.log(heading.innerText)
+console.log(heading.textContent)
+heading.style.fontSize = "100px"
+console.log(heading.innerHTML = "Good Bye")
