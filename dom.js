081e3456ae0f890ec8688d5a6275795f18e95444
Siddharth-M-77 <siddharthmaddheshiya777@gmail.com>
2024-10-22 10:48:30 -0700

    done

--- dom.js
+++ dom.js
@@ -22,12 +22,38 @@
 
 //EventListner in JavaScript
 
-let button = document.querySelector("button")
-console.log(button)
-let heading = document.querySelector("h1")
-console.log(heading)
+// let button = document.querySelector("button")
+// console.log(button)
+// let heading = document.querySelector("h1")
+// console.log(heading)
+
+// button.addEventListener("click",()=>{
+//     heading.innerHTML = "<strike>Kaise ho broooo</strike>"
+// })
+
+//Create a card with HTML & CSS and add a button which shows follow and the heading which shows "Strange ", when the user click on the  button the text of the button chnges to "Unfollow" and the heading chngges to "Friend  and vice versa"
+
+// let btn = document.querySelector("button");
+// let heading = document.querySelector("h1");
+
+// // using if-else 
+// btn.addEventListener("click", () => {
+//   if (heading.innerHTML === "Friends") {
+//     btn.innerHTML = "Follow";
+//     heading.innerHTML = "Strange";
+//     btn.style.backgroundColor="red"
+//   } else {
+//     heading.innerHTML = "Friends";
+//     btn.innerHTML = "Unfollow";
+//     btn.style.backgroundColor="red"
+
+//   }
 
 
-button.addEventListener("click",()=>{
-    heading.innerHTML = "<strike>Kaise ho broooo</strike>"
-})
+// });
+
+//using Destructuring 
+// btn.addEventListener("click", () => {
+//     [heading.innerHTML, btn.innerHTML] =heading.innerHTML==="Friends"?["Strange","Follow"]:["Friends","Unfollow"] 
+    
+//   });
